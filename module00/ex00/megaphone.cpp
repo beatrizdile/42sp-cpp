c8047d2c91f5ef0e1299d61830ee222f41ce144f
Beatriz Dile <bedos-sa@student.42sp.org.br>
2024-03-30 18:32:44 -0300

    ex00

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <cstring>
 
-int main()
+int main(int argc, char **argv)
 {
-    std::cout << "Hello, world!\n";
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+    else
+    {
+        for (int i=1; i < argc; i++)
+        {
+            for (int j=0; j < (int)strlen(argv[i]); j++)
+            {
+                if ((j == 0 || j == (int)strlen(argv[i]) - 1) && argv[i][j] == ' ')
+                    continue;
+                char upper_ch = static_cast<char>(toupper(argv[i][j]));
+                std::cout << upper_ch;
+            }
+            std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
 }
