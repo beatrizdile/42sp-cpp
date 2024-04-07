5b39192f7a964e369c8b9f8bd889fcedce6af686
Beatriz Dile <bedos-sa@student.42sp.org.br>
2024-04-07 15:13:12 -0300

    fix: using toupper from std

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,30 +6,28 @@
 /*   By: bedos-sa <bedos-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 21:05:08 by bedos-sa          #+#    #+#             */
-/*   Updated: 2024/03/30 21:05:09 by bedos-sa         ###   ########.fr       */
+/*   Updated: 2024/04/07 15:10:45 by bedos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cstring>
 
 int main(int argc, char **argv)
 {
-    if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-    else
-    {
-        for (int i=1; i < argc; i++)
-        {
-            for (int j=0; j < (int)strlen(argv[i]); j++)
-            {
-                if ((j == 0 || j == (int)strlen(argv[i]) - 1) && argv[i][j] == ' ')
-                    continue;
-                char upper_ch = static_cast<char>(toupper(argv[i][j]));
-                std::cout << upper_ch;
-            }
-            std::cout << " ";
-        }
-        std::cout << std::endl;
+    if (argc == 1){
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     }
+	int	i = 0;
+	while (++i < argc) 
+    {
+		int	j = 0;
+		while (argv[i][j]) 
+        {
+			argv[i][j] = std::toupper(argv[i][j]);
+			j++;
+		}
+		std::cout << argv[i];
+	}
+	std::cout << std::endl;
+    return (0);
 }
