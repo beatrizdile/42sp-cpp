44343d1ec3d1f1586597fc6c586942f31c1e81d7
Beatriz Dile <bedos-sa@student.42sp.org.br>
2024-05-12 09:39:47 -0300

    module01 ex04

--- module01/ex04/src/main.cpp
+++ module01/ex04/src/main.cpp
@@ -23,16 +23,19 @@ int findSubstring(std::string haystack, std::string needle)
 
 int main(int argc, char** argv)
 {
-	// Check num of params
+	// Check for params
 	if (argc != 4) {
 		std::cout << "Invalid params!" << std::endl;
 		return (0);
 	}
+	std::string needle 		= argv[2];
+	std::string newWord 	= argv[3];
+
 
 	// Open/Create files
 	std::fstream myFile;
 	myFile.open(argv[1], std::ios::in);
-	if (!myFile.is_open()) 
+	if (!myFile.is_open())
 	{
 		std::cerr << "Error opening file!" << std::endl;
 		return (0);
@@ -46,12 +49,34 @@ int main(int argc, char** argv)
 		return (0);
 	}
 
-	// Add content to the ".replace" file
-	std::string line;
-	while (std::getline(myFile, line)){
-		replaceFile << line << std::endl;
-	}
 
+	// Add content to the ".replace" file
+	int			index;
+	std::string	line;
+	while (std::getline(myFile, line))
+	{
+		index = findSubstring(line, needle);
+		if (index == -1)
+			replaceFile << line;
+		else
+		{
+			for (int i = 0; i < (int)line.length(); i++)
+			{
+				if (i == index)
+				{
+					replaceFile << newWord;
+					i += (int)needle.length();
+
+					// Get the next possible match for needle in the same line
+					index += findSubstring(line.substr((index + needle.length()),
+						((int)line.length() - index)), needle) + (int)needle.length();
+				}
+				if (i != (int)line.length())
+					replaceFile << line[i];
+			}
+		}
+		replaceFile << std::endl;
+	}
 	myFile.close();
 	replaceFile.close();
 }
