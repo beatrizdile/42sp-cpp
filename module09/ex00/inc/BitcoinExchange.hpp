d1fc163511bbd8c7290dd1788191eb82e44bd21c
Beatriz Dile <bedos-sa@student.42sp.org.br>
2024-11-02 18:28:01 -0300

    init module09 ex00

---
+++ module09/ex00/inc/BitcoinExchange.hpp
@@ -0,0 +1,15 @@
+#ifndef BITCOIN_HPP
+#define BITCOIN_HPP
+
+#include <stdexcept>
+#include <algorithm>
+#include <iostream>
+#include <map>
+
+class BitcoinExchange
+{
+    private:
+    public:
+};
+
+#endif
---
+++ module09/ex00/src/main.cpp
@@ -0,0 +1,9 @@
+#include "easyfind.hpp"
+
+int main()
+{
+	// Your program must take a file as argument.
+	// Each line in this file must use the following format: "date | value"
+	// A valid date will always be in the following format: Year-Month-Day
+	// A valid value must be either a float or a positive integer, between 0 and 1000.
+}
