d556e282a69933f35f19c0bbc2dfb3732254d687
Beatriz Dile <bedos-sa@student.42sp.org.br>
2024-11-24 17:29:25 -0300

    init module09 ex02

---
+++ module09/ex02/inc/PmergeMe.hpp
@@ -0,0 +1,15 @@
+#ifndef PmergeMe_HPP
+#define PmergeMe_HPP
+
+#include <sys/stat.h>
+#include <stdexcept>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stack>
+#include <string>
+
+
+#endif
---
+++ module09/ex02/src/main.cpp
@@ -0,0 +1,10 @@
+#include "PmergeMe.hpp"
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2) {
+		std::cerr << "Error: must take a string as argument" << std::endl;
+		return 1;
+	}
+	(void) argv;
+}
